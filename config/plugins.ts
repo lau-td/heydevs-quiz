f577ad08bd6a5688e74a75f31cf6d40682083616
lau.td <lau.td.developer@gmail.com>
2024-07-05 10:43:49 +0700

    build: change email provider to amazon ses

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,11 +1,26 @@
 export default ({ env }) => ({
+  // email: {
+  //   config: {
+  //     provider: "mailgun",
+  //     providerOptions: {
+  //       key: env("MAILGUN_API_KEY"), // Required
+  //       domain: env("MAILGUN_DOMAIN"), // Required
+  //       url: env("MAILGUN_URL", "https://api.mailgun.net"), //Optional. If domain region is Europe use 'https://api.eu.mailgun.net'
+  //     },
+  //     settings: {
+  //       defaultFrom: env("EMAIL_DEFAULT_FROM"),
+  //       defaultReplyTo: env("EMAIL_DEFAULT_REPLY_TO"),
+  //     },
+  //   },
+  // },
+
   email: {
     config: {
-      provider: "mailgun",
+      provider: "amazon-ses",
       providerOptions: {
-        key: env("MAILGUN_API_KEY"), // Required
-        domain: env("MAILGUN_DOMAIN"), // Required
-        url: env("MAILGUN_URL", "https://api.mailgun.net"), //Optional. If domain region is Europe use 'https://api.eu.mailgun.net'
+        key: env("AWS_SES_ACCESS_KEY"),
+        secret: env("AWS_SES_SECRET_KEY"),
+        amazon: `https://email.${env("AWS_SES_REGION")}.amazonaws.com`,
       },
       settings: {
         defaultFrom: env("EMAIL_DEFAULT_FROM"),
